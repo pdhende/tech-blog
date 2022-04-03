c0f103d1c8fbe014e0cb5773877bd9ec69a8b71b
pdhende <pdhendework@gmail.com>
2022-04-03 16:01:08 -0400

    Updated homeRoutes to build array of js objects and display them on hoempage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,27 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
+
+    const allPosts = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
     
+  //  const posts =  res.json(allPosts);
+  //  console.log(posts);
+    // Serialize data so the template can read it
+    const posts = allPosts.map((post) => post.get({ plain: true }));
+    // console.log(posts);
     // Pass serialized data and session flag into template
     res.render('homepage', { 
+      posts,
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -14,35 +29,12 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/project/:id', async (req, res) => {
-//   try {
-//     const projectData = await Project.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
-
-//     const project = projectData.get({ plain: true });
-
-//     res.render('project', {
-//       ...project,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      // include: [{ model: Project }],
     });
 
     const user = userData.get({ plain: true });
